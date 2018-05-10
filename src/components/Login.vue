850bf50ef175e8a076876c068221d249fdcebade
tuzkimo <tuzkimo@gmail.com>
2018-05-10 09:58:32 +0800

    feat: add components

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <input v-model="username" placeholder="username...">
+      <input type="password" v-model="password" placeholder="password...">
+      <button type="submit">login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        username: '',
+        password: ''
+      }
+    },
+
+    methods: {
+      login () {
+
+      }
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Index from '@/components/Index'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {
+      path: '/',
+      component: Index
+    },
+
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
 })
